3fc584884cb4c87e626b02dd181a97bf4c3b556a
Alexander <aleksandrchistov.ru@yandex.ru>
2020-12-10 22:25:51 +0700

    set up the build

---
+++ lesson_8/public/js/index.js
@@ -0,0 +1,6 @@
+import Vue from './vue';
+import Search from './search';
+import ShoppingCart from './shoppingCart';
+import Products from './products';
+import ErrorHandler from './errorHandler';
+import App from './main';
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    entry: './lesson_8/public/js/index.js',
+    output: {
+        filename: './bundle.js',
+    },
+};
